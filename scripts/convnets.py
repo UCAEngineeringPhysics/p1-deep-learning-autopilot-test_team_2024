7776f453a7dfc52bfb6d8cb2860cbc94522a7476
LinZhang@UCA <88195432+linzhangUCA@users.noreply.github.com>
2024-11-25 16:10:57 -0600

    Update convnets.py

--- scripts/convnets.py
+++ scripts/convnets.py
@@ -32,12 +32,32 @@ class DonkeyNet(nn.Module):
 
 ### START CODING HERE ###
 class AutopilotNet(nn.Module):
+
     def __init__(self):
         super().__init__()
-        self.conv1 = None
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=(5, 5), stride=(2, 2))
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=(5, 5), stride=(2, 2))
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=(3, 3))
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=(3, 3))
+        
+        self.fc1 = nn.Linear(128*6*8, 128)  # (64*30*30, 128) for 300x300 images
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 2)
 
     def forward(self, x):  # 176x208x3 -> 2
-        x = None
-        return x
+        x = F.relu(self.conv1(x))  # (176 - 5) / 2 + 1 = 86   (208 - 5) / 2 + 1 = 102
+        x = self.pool(x)  # (86 - 2) / 2 + 1 = 43   (102 - 2) / 2 + 1 = 51
+        x = F.relu(self.conv2(x))  # (43 - 5) / 2 + 1 = 20   (51 - 5) / 2 + 1 = 24
+        x = self.pool(x)  # (20 - 2) / 2 + 1 = 10   (24 - 2) / 2 + 1 = 12
+        x = F.relu(self.conv3(x))  # (10 - 3) / 1 + 1 = 8   (12 - 3) / 1 + 1 = 10
+        x = F.relu(self.conv4(x))  # (8 - 3) / 1 + 1 = 6   (10 - 3) / 1 + 1 = 8
+        x = x.view(-1, 128 * 6 * 8)  # Flatten, 128*6*8
+        x = F.relu(self.fc1(x))  # 128
+        x = F.relu(self.fc2(x))  # 128
+        y = F.relu(self.fc3(x))  # 2
+
+        return y
+
 ### END CODING HERE ###
 
